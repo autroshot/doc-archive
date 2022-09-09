82d461b680c0612812bf171ea54d595e40253b9c
autroshot <autroshot@gamil.com>
2022-09-09 21:36:01 +0900

    설정: 문서 전용 모드

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,14 +38,9 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          routeBasePath: "/",
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
+        blog: false,
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
