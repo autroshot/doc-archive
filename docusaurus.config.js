4e3a840ce7abc3740dfe5bf0a2b9505a077d0237
autroshot <autroshot@gamil.com>
2022-11-03 22:17:04 +0900

    docusaurus-lunr-search 패키지 추가 및 적용

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -196,7 +196,15 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
-  plugins: [['@docusaurus/plugin-ideal-image', { disableInDev: false }]],
+  plugins: [
+    ['@docusaurus/plugin-ideal-image', { disableInDev: false }],
+    [
+      require.resolve('docusaurus-lunr-search'),
+      {
+        languages: ['ko', 'en'],
+      },
+    ],
+  ],
 };
 
 module.exports = config;
