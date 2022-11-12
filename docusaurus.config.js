4f570e17f0cdef63e7d98e94c99cebc0840c04eb
autroshot <autroshot@gamil.com>
2022-11-12 15:51:56 +0900

    설정: docusaurus-search-local 적용

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -215,8 +215,17 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
-  plugins: [
-    ['@docusaurus/plugin-ideal-image', { disableInDev: false }],
+  plugins: [['@docusaurus/plugin-ideal-image', { disableInDev: false }]],
+  themes: [
+    [
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      require.resolve('@easyops-cn/docusaurus-search-local'),
+      {
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        language: ['en', 'ko'],
+      },
+    ],
   ],
 };
 
