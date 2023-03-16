0c02e06428987b955ff5f7060cf66a6242ae1421
autroshot <autroshot@gmail.com>
2023-03-16 10:42:43 +0900

    리팩터링: 인덱스 페이지: 반환 타입 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
   );
 }
 
-export default function Home(): JSX.Element {
+export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
