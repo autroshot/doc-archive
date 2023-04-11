fd7f3000d7559d3d61da22490aafb008bcded2aa
autroshot <autroshot@gmail.com>
2023-04-11 20:02:20 +0900

    리팩터링: 일부 태그를 CodeSandbox 컴포넌트로 대체

---
+++ src/components/codeSandbox.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function CodeSandbox({ title, modulePath }) {
+  let moduleQueryString = '';
+  if (modulePath) {
+    const convertedModulePath = modulePath.replace(/\//g, '%2F');
+    moduleQueryString = `&module=${convertedModulePath}`;
+  }
+
+  return (
+    <iframe
+      src={`https://codesandbox.io/embed/${title}?codemirror=1&fontsize=14&hidenavigation=1&view=editor${moduleQueryString}`}
+      style={{
+        width: '100%',
+        height: '500px',
+        border: '0',
+        borderRadius: '4px',
+        overflow: 'hidden',
+      }}
+      title={title}
+      allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+      sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+    />
+  );
+}
--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,7 +1,9 @@
 import MDXComponents from '@theme-original/MDXComponents';
+import CodeSandbox from '/src/components/codeSandbox.jsx';
 import Image from '/src/components/image.jsx';
 
 export default {
   ...MDXComponents,
   Image,
+  CodeSandbox,
 };
